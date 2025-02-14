f5a8458b9c9703384b82284a2c016508a570d290
Ian Lewis <ian@ianlewis.org>
2025-02-14 07:18:51 +0000

    chore: update jest config
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- jest.config.js
+++ jest.config.js
@@ -22,17 +22,6 @@ export default {
   coverageDirectory: "./coverage",
   coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
   coverageReporters: ["json-summary", "text", "lcov"],
-  // Uncomment the below lines if you would like to enforce a coverage threshold
-  // for your action. This will fail the build if the coverage is below the
-  // specified thresholds.
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 100,
-  //     functions: 100,
-  //     lines: 100,
-  //     statements: 100
-  //   }
-  // },
   extensionsToTreatAsEsm: [".ts"],
   moduleFileExtensions: ["ts", "js"],
   preset: "ts-jest",
